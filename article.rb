e4b9082804beff69a125ac172da30c47b5df1816
Michalina Sieradzka <misia.sieradzka@gmail.com>
2014-10-22 13:48:27 +0200

    [#4] Article Class

--- article.rb
+++ article.rb
@@ -1,42 +1,39 @@
 class Article
   def initialize(title, content, author)
-    @title = title
-    @content = content
-    @author = author
-    @likes = 0
-    @dislikes = 0
+    @title, @content, @author = title, content, author
+    @likes = @dislikes = 0
     @created_at = Time.now()
   end
-  def title
-    @title
-  end
-  def content
-    @content
-  end
-  def author
-    @author
-  end
+
+  attr_accessor :title, :content, :author
   def like!
     @likes += 1
   end
+
   def dislike!
     @dislikes += 1
   end
+
   def positive_votes
     @likes - @dislikes
   end
+
   def votes
     @likes + @dislikes
   end
+
   def shortened_to(limit)
     @content.slice(0, limit-3) + "..."
   end
+
   def include?(word)
     @content.include?(word)
   end
+
   def words
     @content.split
   end
+
   def distinct_words
     words | words
   end
