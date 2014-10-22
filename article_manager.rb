9acdb3aa24a41183a3cfbef54e602086c5877663
Michalina Sieradzka <misia.sieradzka@gmail.com>
2014-10-22 14:31:41 +0200

    [#5] Article Manager

--- article_manager.rb
+++ article_manager.rb
@@ -1,45 +1,55 @@
 class ArticleManager
-  def initialize(array)
-    @array = array
+  def initialize(articles)
+    @articles = articles
   end
+
   def worst_articles
-    @array.sort {|a,b| a.positive_votes <=> b.positive_votes}
+    @articles.sort {|a,b| a.positive_votes <=> b.positive_votes}
   end
+
   def best_articles
-    @array.sort {|a,b| b.positive_votes <=> a.positive_votes}
+    @articles.sort {|a,b| b.positive_votes <=> a.positive_votes}
   end
+
   def worst_article
-    worst_articles[0]
+    worst_articles.first
   end
+
   def best_article
-    best_articles[0]
+    best_articles.first
   end
+
   def most_popular_article
-    arr = @array.sort {|a,b| b.votes <=> a.votes}
-    arr[0]
+    sorted = @articles.sort {|a,b| b.votes <=> a.votes}
+    sorted.first
   end
+
   def include?(pattern)
     y=false
-    @array.each{|x| if x.include?(pattern) then y=true end}
+    @articles.each{|x| if x.include?(pattern) then y=true end}
     y
   end
+
   def authors
-    tab = []
-    @array.each{|x| tab = tab + [x.author]}
-    tab | tab
+    list_of_authors = []
+    @articles.each{|x| list_of_authors += [x.author]}
+    list_of_authors | list_of_authors
   end
+
   def number_of_authors
-    return authors.size
+    authors.size
   end
+
   def votes
     sum = 0
-    @array.each{|x| sum += x.votes}
+    @articles.each{|x| sum += x.votes}
     sum
   end
+
   def to_s
-  s = ""
-    for i in 0..@array.size - 1
-      s = s + (i+1).to_s + ". " + "Title: " + @array[i].title + "\n" + "Content: " + @array[i].content + "\n" + "Author: " + @array[i].author + "\n\n"
+    s = ""
+    for i in 0..@articles.size - 1
+      s = s + (i+1).to_s + ". " + "Title: " + @articles[i].title + "\n" + "Content: " + @articles[i].content + "\n" + "Author: " + @articles[i].author + "\n\n"
     end
     s
   end
