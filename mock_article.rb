b1bbf8f9a799d5e0179f81f25244f6cb92f58316
Michalina Sieradzka <misia.sieradzka@gmail.com>
2014-10-30 13:24:50 +0100

    [#6] MockArticle Class

--- mock_article.rb
+++ mock_article.rb
@@ -12,27 +12,27 @@ class MockArticle < Article
   end
 
   def generate_author
-    first_name = WORD_LIST[random_number % 10].capitalize
-    second_name = WORD_LIST[random_number % 10].capitalize
+    first_name = WORD_LIST.sample.capitalize
+    second_name = WORD_LIST.sample.capitalize
     new_author = first_name + " " + second_name
   end
 
   def generate_title
-    how_many = random_number % 5
-    new_title = WORD_LIST[random_number % 10].capitalize
-    how_many.times {new_title += " " + WORD_LIST[random_number%10]}
+    how_many = Random.rand(5)
+    new_title = WORD_LIST.sample.capitalize
+    how_many.times {new_title += " " + WORD_LIST.sample}
     new_title
   end
 
   def generate_sentence
-    how_many = random_number % 6 + 4
-    new_sentence = WORD_LIST[random_number % 10].capitalize
-    how_many.times {new_sentence += " " + WORD_LIST[random_number%10]}
+    how_many = Random.rand(6) + 4
+    new_sentence = WORD_LIST.sample.capitalize
+    how_many.times {new_sentence += " " + WORD_LIST.sample}
     new_sentence += "."
   end
 
   def generate_content
-    how_many = random_number % 5 + 1
+    how_many = Random.rand(5) + 1
     new_content = ""
     how_many.times {new_content += generate_sentence + " "}
     new_content.chop
